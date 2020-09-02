dca396106b72d683368d52abd75a6c7bee676190
jlli <1986626374@qq.com>
2020-09-03 01:33:19 +0800

    reactor操作

--- src/test/java/reactorTest/Reactor.java
+++ src/test/java/reactorTest/Reactor.java
@@ -1,6 +1,7 @@
 package reactorTest;
 
 import org.junit.Test;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public class Reactor {
@@ -8,5 +9,8 @@ public class Reactor {
     @Test
     public void test(){
         Mono.just("1").map(a -> a + "1").subscribe(System.out::println);
+        Mono.just("1").flatMap(a -> Mono.just(a + "1")).subscribe(System.out::println);
+        Flux.just("1").map(a -> a + "1").subscribe(System.out::println);
+        Flux.just("1").flatMap(a -> Flux.just(a + "1")).subscribe(System.out::println);
     }
 }
